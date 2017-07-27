8cfd4e77b9d310a1fefe5f3b646f1cce83d46fb8
Daniel Wiehoff <daniel.wiehoff+flatiron@gmail.com>
2017-07-27 14:39:24 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,33 @@
+// know how many times it will run
+function forLoop(array) {
+  for (let i=0; i<25; i++) {
+    if (i == 1)
+      array.push("I am " + i + " strange loop.")
+    else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array;
+}
+
+// don't know how many times it will loop; condition is dependent on dynamic function or return value
+// can also be used in place of for loop (change condition on each pass)
+function whileLoop(num) {
+  while (num > 0) { // should actually be >= to count down to 0
+    console.log(num);
+    num--;
+  }
+  return 'done';
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+// loop body executed at least once
+function doWhileLoop(array) {
+  do {
+    array.pop();
+  } while (array.length > 0 || maybeTrue());
+  return array;
+}
